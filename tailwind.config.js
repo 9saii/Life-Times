23249d39b46d39b78d4e85b9f1ed4f5497f368e3
Suru Gowtham Sai <130141529+9saii@users.noreply.github.com>
2025-03-04 12:51:24 +0530

    add 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Montserrat"', 'sans-serif'],
+      },
+    },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '1.75rem',
+      '3xl': '2rem',
+      '4xl': '2.25rem',
+      '5xl': '2.5rem',
+      '6xl': '2.75rem',
+      '7xl': '3rem',
+      '8xl': '3.25rem',
+      '9xl': '3.5rem',
+    },
   },
   plugins: [],
 };
